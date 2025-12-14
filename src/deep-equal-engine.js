ff75757653cddbf1059cc39519952280b22dc10d
Stanislav <stalevs@gmail.com>
2025-12-14 14:29:00 +0100

    add cmparator engine

---
+++ src/deep-equal-engine.js
@@ -0,0 +1,34 @@
+import {PrimitiveComparator} from "./primitive-comparator";
+import {ArrayComparator} from "./array-comparator";
+import {ObjectComparator} from "./object-comparator";
+
+export class DeepEqualEngine {
+  constructor() {
+    this.comparators = [
+      new PrimitiveComparator(),
+      new ArrayComparator(),
+      new ObjectComparator(),
+    ];
+    this.visited = new WeakSet();
+  }
+
+  deepEqual(a, b) {
+    if(typeof a === 'object' && a !== null) {
+      if(this.visited.has(a)) {
+        return a===b;
+      }
+      this.visited.add(a);
+    }
+    const comparator = this.findComparator(a);
+
+    if(!comparator) {
+      throw new Error(`Cannot find comparator for ${typeof a}`);
+    }
+
+    return comparator.compare(a, b, this);
+  }
+
+  findComparator(value) {
+    return this.comparators.find((comparator)=> comparator.canHandle(value))
+  }
+}
