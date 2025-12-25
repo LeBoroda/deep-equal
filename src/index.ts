9693b5c696a25a6cb6342a75f6406ff2f7cc42c2
Stanislav <stalevs@gmail.com>
2025-12-25 22:50:55 +0100

    add new use cases

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,19 @@ console.log(greatEqualizer.deepEqual([1, 2], [1, 2]));
 console.log(
   greatEqualizer.deepEqual(new Date('2323-01-11'), new Date('2323-01-12')),
 );
+console.log(
+  greatEqualizer.deepEqual('I am GROOT!', {
+    name: 'Groot',
+    phrase: 'I am GROOT!',
+  }),
+);
+
+//Create object with recursive link 'by mistake'
+const spaceOverlord: any = {
+  name: 'Peter',
+  lastName: 'Quill',
+  nickname: null,
+};
+spaceOverlord.nickname = spaceOverlord;
+
+console.log(greatEqualizer.deepEqual(spaceOverlord, spaceOverlord.nickname));
