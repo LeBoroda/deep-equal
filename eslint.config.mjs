384e83dd49a33d4e505858de3acd465c876ca3c7
Stanislav <stalevs@gmail.com>
2025-12-25 21:55:19 +0100

    setup eslint for ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,22 +3,34 @@ import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import jest from 'eslint-plugin-jest';
 import eslintRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default defineConfig([
   eslintRecommended,
   globalIgnores(['./dist/*.js', './dist/*.ts']),
   {
     files: ['**/*.{js,mjs,cjs,ts,tsx}'],
-    plugins: { js },
+    plugins: {
+      js,
+      '@typescript-eslint': tseslint,
+    },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: globals.node,
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
     rules: {
       semi: ['error', 'always'],
       'no-useless-escape': ['error', { allowRegexCharacters: ['-'] }],
     },
   },
   {
-    files: ['src/**/*.test.js'],
+    files: ['src/**/*.test.{js,ts}'],
     ...jest.configs['flat/recommended'],
   },
   {
